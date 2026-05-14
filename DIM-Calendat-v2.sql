26374b573ed9bbd1e41799319236fd89304abfcd
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-14 15:25:31 +0300

    Add calendar dimension CSV and SQL
    Add DIM-Calendat-v2.csv containing daily date dimension rows for 2014 and DIM-Calendat-v2.sql which exports date attributes from AdventureWorksDW2019.dbo.DimDate. The SQL selects DateKey, FullDateAlternateKey as Date, EnglishDayNameOfWeek as Day, WeekNumberOfYear as Week, EnglishMonthName/MonthShort, MonthNumberOfYear as MonthNo, CalendarQuarter as Quarter and CalendarYear as Year, filtering CalendarYear >= 2014; other date attributes remain commented out.

---
+++ DIM-Calendat-v2.sql
@@ -0,0 +1,28 @@
+SELECT 
+  Top (1000) [DateKey], 
+  [FullDateAlternateKey] AS Date 
+  --,[DayNumberOfWeek]
+  , 
+  [EnglishDayNameOfWeek] AS Day 
+  -- ,[SpanishDayNameOfWeek]
+  -- ,[FrenchDayNameOfWeek]
+  -- ,[DayNumberOfMonth]
+  -- ,[DayNumberOfYear]
+  , 
+  [WeekNumberOfYear] AS Week, 
+  [EnglishMonthName] As Month, 
+  LEFT([EnglishMonthName], 3) AS MonthShort 
+  -- ,[SpanishMonthName]
+  -- ,[FrenchMonthName]
+  , 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  -- ,[CalendarSemester]
+  -- ,[FiscalQuarter]
+  -- ,[FiscalYear]
+  -- ,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate]
+WHERE
+   CalendarYear >= 2014
